e470316967f5dc7c95539e435ac6e58944484456
wfei <wfei@WFEI-PC.thunisoft.com>
2013-10-15 17:55:00 +0800

    用户放到session中

--- src/com/feihome/logic/user/UserLogic.java
+++ src/com/feihome/logic/user/UserLogic.java
@@ -1,7 +1,5 @@
 package com.feihome.logic.user;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,45 +14,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.feihome.model.TBlog;
 import com.feihome.model.TUser;
 import com.feihome.service.CommonService;
 
 @Controller
 @RequestMapping("/user")
 public class UserLogic {
-	
+
 	@Autowired
 	@Qualifier("commonService")
 	CommonService commonService;
-	
+
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	@ResponseBody
 	public String login(
-			@RequestParam(value = "username", required = true) String user,
-			@RequestParam(value = "passwords", required = true) String passwords) {
+			@RequestParam(value = "username", required = true) String username,
+			@RequestParam(value = "passwords", required = true) String passwords,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 		JSONObject result = new JSONObject();
-		result.put("result", commonService.isUser(user, passwords));
+		TUser user = commonService.getUser(username,passwords);
+		if(user != null){
+			request.getSession().setAttribute("user",user);
+		}
+		result.put("result", user != null);
 		return result.toString();
 	}
-	
+
 	@RequestMapping(value = "loginByUUID", method = RequestMethod.POST)
 	@ResponseBody
 	public String loginByUUID(
-			@RequestParam(value = "sercet", required = true) String sercetCode){
+			@RequestParam(value = "sercet", required = true) String sercetCode) {
 		JSONObject result = new JSONObject();
 		System.out.println(2);
 		result.put("result", 1);
 		return result.toString();
 	}
-	
+
 	@RequestMapping(value = "signup")
-	public ModelAndView login(HttpServletRequest request,
+	public ModelAndView signup(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		TUser user = (TUser) request.getSession().getAttribute("user");
-		if(user != null){
+		if (user != null) {
 			request.getSession().removeAttribute("user");
 		}
-        return new ModelAndView("user/signup");  
+		return new ModelAndView("user/signup");
 	}
 }
