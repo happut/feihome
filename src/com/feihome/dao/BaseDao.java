7729c41dd6a433719c0dbf11e6d005d22bb45c51
wfei <wfei@WFEI-PC.thunisoft.com>
2013-10-15 17:54:36 +0800

    增加获取用户方法

--- src/com/feihome/dao/BaseDao.java
+++ src/com/feihome/dao/BaseDao.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.feihome.model.TBlog;
+import com.feihome.model.TUser;
 
 public class BaseDao extends JdbcDaoSupport{
 
@@ -27,5 +28,29 @@ public class BaseDao extends JdbcDaoSupport{
 				return blog;
 			}
 		});
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public TUser getUser(String username, String passwords) {
+		List<TUser> users = getJdbcTemplate().query("select * from t_user where C_USERNAME = ? and C_PASSWORD = ? " ,new Object[]{username,passwords},
+				new RowMapper() {
+					@Override
+					public Object mapRow(ResultSet rs, int arg1)
+							throws SQLException {
+						TUser user = new TUser();
+						user.setNId(rs.getInt("N_ID"));
+						user.setCEmail(rs.getString("C_EMAIL"));
+						user.setCPassword(rs.getString("C_PASSWORD"));
+						user.setCUsername(rs.getString("C_USERNAME"));
+						user.setDtCreateTime(rs.getDate("DT_CREATE_TIME"));
+						return user;
+					}
+				});
+		if(users.isEmpty()){
+			return null;
+		}else{
+			return users.get(0);
+		}
 	}
 }
