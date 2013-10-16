3de048f8f9363bb7161a25616825882a43941404
wfei <wfei@WFEI-PC.thunisoft.com>
2013-10-16 17:48:56 +0800

    测试界面

--- src/com/feihome/logic/blog/BlogLogic.java
+++ src/com/feihome/logic/blog/BlogLogic.java
@@ -1,14 +1,20 @@
 package com.feihome.logic.blog;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.feihome.model.TBlog;
@@ -33,5 +39,37 @@ public class BlogLogic {
         List<TBlog> data = commonService.getBlogs();
         
         return new ModelAndView("blog/list","data",data);  
+	}
+	
+	
+	@RequestMapping(value = "addBlog")
+	public ModelAndView addBlog(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+        return new ModelAndView("blog/addBlog");  
+	}
+	
+	@RequestMapping(value = "addBlogReq", method = RequestMethod.POST)
+	@ResponseBody
+	public String login(
+			@RequestParam(value = "title", required = true) String title,
+			@RequestParam(value = "content", required = true) String content,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		JSONObject result = new JSONObject();
+		TUser user = (TUser) request.getSession().getAttribute("user");
+		if(user == null){
+			throw new Exception("session中未保存user信息");
+		}
+		TBlog blog = new TBlog();
+		blog.setCContent(content);
+		blog.setCTitle(title);
+		blog.setDtCreatetime(new Date());
+		blog.setDtEdittime(new Date());
+		blog.setNType(0);
+		blog.setNUserid(user.getNId());
+		
+		boolean isSuccess = commonService.createBlog(blog);
+		result.put("result", user != null);
+		return result.toString();
 	}
 }
--- web/jsp/blog/list.jsp
+++ web/jsp/blog/list.jsp
@@ -8,6 +8,7 @@
 <script src="../js/jquery-2.0.3.js"></script>
 <script src="../js/bootstrap.js"></script>
 <link href="../css/bootstrap.css" rel="stylesheet">
+<link href="../jsp/blog/css/common.css" rel="stylesheet">
 <script type="text/javascript">
 	$(document).ready(function(){
 		
@@ -17,6 +18,7 @@
 <body>
 	<nav class="navbar navbar-default navbar-static-top" role="navigation">
 		<a class="navbar-brand" href="#">Blog</a>
+		<p class="navbar-text"><a href="../blog/addBlog.action">新增博客</a></p>
 		<c:if test="${!empty sessionScope.user.CUsername}">
 			<p class="navbar-text pull-right" id="logined">Signed in as <c:out value='${user.CUsername}' />  <a href="../user/signup.action?ref=blog_list">Sign out</a></p>
 		</c:if>
@@ -24,6 +26,7 @@
 			<p class="navbar-text pull-right" id="login"><a href="../user/signup.action?ref=blog_list">Sign in</a></p>
 		</c:if>
 	</nav>
+	    <div id="wrap">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
@@ -43,5 +46,11 @@
 			</div>
 		</div>
 	</div>
+	</div>
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted">Copyright © 2013 Wfei.de.bug.(Happut). All rights reserved. </p>
+      </div>
+    </div>
 </body>
 </html>
