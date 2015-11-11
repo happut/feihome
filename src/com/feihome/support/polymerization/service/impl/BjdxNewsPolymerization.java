3d5cef6cdbde598616b613271b34e2b81b6e05a0
wangfei <littlewangfei@vip.qq.com>
2015-11-11 15:35:17 +0800

    Signed-off-by: wangfei <littlewangfei@vip.qq.com>

--- src/com/feihome/support/polymerization/service/impl/BjdxNewsPolymerization.java
+++ src/com/feihome/support/polymerization/service/impl/BjdxNewsPolymerization.java
@@ -1,8 +1,12 @@
 package com.feihome.support.polymerization.service.impl;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -16,21 +20,35 @@ public class BjdxNewsPolymerization implements IPolymerization {
 
 	@Override
 	public List<TPolymerizationElement> pullPolymerizationInfo() {
+		List<TPolymerizationElement> dataList = new ArrayList<TPolymerizationElement>();
 		Document doc;
 		try {
 			doc = Jsoup.connect("http://www.bjdx.gov.cn/zmhd/wsgssx/").get();
 
-		Elements select = doc.select(".zfwj_ul li");
-		for(org.jsoup.nodes.Element e : select){
-			System.out.println(e.child(0).attr("title"));
-			System.out.println("http://www.bjdx.gov.cn"+e.child(0).attr("href"));
-			System.out.println(e.child(1).text());
-		}
-		
+			Elements select = doc.select(".zfwj_ul li");
+			for (org.jsoup.nodes.Element e : select) {
+				TPolymerizationElement element = new TPolymerizationElement();
+				
+				String url = e.child(0).attr("href");
+				String date = e.child(1).text().replace("[", "").replace("]", "");
+				
+				try {
+					element.setDate(DateUtils.parseDate(date, new String[]{"yyyy-MM-dd"}));
+				} catch (ParseException e1) {
+					element.setDate(null);
+				}
+				
+				element.setTitle(e.child(0).attr("title"));
+				element.setType(1);
+				element.setUrl("http://www.bjdx.gov.cn" + url);
+				element.setId(NumberUtils.toInt(url.substring(
+						url.lastIndexOf("/") + 1, url.lastIndexOf("."))));
+				dataList.add(element);
+			}
+
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		return null;
+		return dataList;
 	}
 }
