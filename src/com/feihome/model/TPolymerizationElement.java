c91100002920dab83d6c55bf7786d5ceb7dd1c36
wangfei <littlewangfei@vip.qq.com>
2015-11-11 15:35:06 +0800

    聚合相关代码
    Signed-off-by: wangfei <littlewangfei@vip.qq.com>

--- src/com/feihome/model/TPolymerizationElement.java
+++ src/com/feihome/model/TPolymerizationElement.java
@@ -1,5 +1,78 @@
 package com.feihome.model;
 
-public class TPolymerizationElement {
+import java.util.Date;
 
+public class TPolymerizationElement {
+	private Integer id;
+	private Integer type;
+	private Date date;
+	private String title;
+	private String url;
+	private String ext1;
+	private String ext2;
+	private String ext3;
+	private String ext4;
+	private String ext5;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getExt1() {
+		return ext1;
+	}
+	public void setExt1(String ext1) {
+		this.ext1 = ext1;
+	}
+	public String getExt2() {
+		return ext2;
+	}
+	public void setExt2(String ext2) {
+		this.ext2 = ext2;
+	}
+	public String getExt3() {
+		return ext3;
+	}
+	public void setExt3(String ext3) {
+		this.ext3 = ext3;
+	}
+	public String getExt4() {
+		return ext4;
+	}
+	public void setExt4(String ext4) {
+		this.ext4 = ext4;
+	}
+	public String getExt5() {
+		return ext5;
+	}
+	public void setExt5(String ext5) {
+		this.ext5 = ext5;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
 }
--- src/com/feihome/support/polymerization/Job/PolymerizationJob.java
+++ src/com/feihome/support/polymerization/Job/PolymerizationJob.java
@@ -1,26 +1,42 @@
 package com.feihome.support.polymerization.Job;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
+import com.feihome.logic.blog.BlogDao;
+import com.feihome.model.TBlog;
 import com.feihome.support.polymerization.service.IPolymerization;
 import com.feihome.support.utils.SpringContextUtil;
 
 @Repository
 public class PolymerizationJob {
 
-	@Scheduled(cron = "0/30 * * * * ? ")
-	public void polymerization() {
+	@Autowired
+	@Qualifier("blogDao")
+	private BlogDao dao;
+
+	@Scheduled(cron = "0/30 1 * * * ? ")
+	public void polymerization() throws SQLException {
 		System.out.println("test");
 
-		Map<String, IPolymerization> beansByType = SpringContextUtil
+		Map<String, IPolymerization> beans = SpringContextUtil
 				.getBeansByType(IPolymerization.class);
 
-		for (String s : beansByType.keySet()) {
-			beansByType.get(s).pullPolymerizationInfo();
+		for (String s : beans.keySet()) {
+			beans.get(s).pullPolymerizationInfo();
 		}
+		// 返回单行记录，使用Map
+		System.out.println("使用Map处理单行记录！");
+		List<TBlog> map = dao.getRunner().query("select * from t_blog",
+				new BeanListHandler(TBlog.class));
+		System.out.println();
 
 	}
 }
