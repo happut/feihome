8bfc4bcd3bdf3a52bacb5a91481428e4b5e88c0a
wangfei <littlewangfei@vip.qq.com>
2016-07-25 17:31:45 +0800

    去掉queryrunner
    Signed-off-by: wangfei <littlewangfei@vip.qq.com>

--- src/com/feihome/dao/BaseDao.java
+++ src/com/feihome/dao/BaseDao.java
@@ -2,7 +2,6 @@ package com.feihome.dao;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbutils.QueryRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,8 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class BaseDao{
 
 	private JdbcTemplate jdbcTemplate;
-
-	private QueryRunner runner;
 	/**
 	 * Set the JDBC DataSource to be used by this DAO.
 	 */
@@ -19,10 +16,6 @@ public class BaseDao{
 	public void setDataSource(@Qualifier("dataSource")DataSource dataSource) {
 		if (this.jdbcTemplate == null || dataSource != this.jdbcTemplate.getDataSource()) {
 			this.jdbcTemplate = new JdbcTemplate(dataSource);
-		}
-		
-		if (this.runner == null || dataSource != this.runner.getDataSource()) {
-			this.runner = new QueryRunner(dataSource);
 		}
 	}
 	
@@ -33,15 +26,4 @@ public class BaseDao{
 	public JdbcTemplate getJdbcTemplate() {
 	  return this.jdbcTemplate;
 	}
-
-	/**
-	 * Return the runner(dbutils) for this DAO,
-	 * pre-initialized with the DataSource or set explicitly.
-	 */
-	public QueryRunner getRunner() {
-		return runner;
-	}
-	
-	
-	
 }
