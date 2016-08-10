8883254d4600af7b324c8e134c5e51fdd8e6fb79
wangfei <littlewangfei@gmail.com>
2016-08-10 15:26:35 +0800

    减少定时器频率
    Signed-off-by: wangfei <littlewangfei@gmail.com>

--- src/com/feihome/support/time/UserEntireJob.java
+++ src/com/feihome/support/time/UserEntireJob.java
@@ -9,8 +9,11 @@ import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.SLF4JLogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
 import com.feihome.logic.blog.BlogService;
@@ -23,7 +26,11 @@ public class UserEntireJob {
 	@Qualifier("blogService")
 	private BlogService blogService;
 	
+	Log log = SLF4JLogFactory.getLog(getClass());
+	
+	@Scheduled(cron = "0/30 1 * * * ? ")
 	private void refreshCache() {
+	    log.info("开始刷新UserEntireJob缓存");
 		Cache cache = CacheManager.getInstance().getCache("userEntire");
 		if(blogService == null){
 			return;
@@ -39,5 +46,6 @@ public class UserEntireJob {
 		
 		Element userMapEle = new Element("userMap", userMap);
 		cache.put(userMapEle);
+		log.info("结束刷新UserEntireJob缓存");
 	}
 }
