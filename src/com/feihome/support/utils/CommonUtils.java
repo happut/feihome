735d49df7fcd6ec648ff3a3f5809d09ebf19712c
wangfei <littlewangfei@gmail.com>
2016-08-24 18:31:48 +0800

    UUID生成
    Signed-off-by: wangfei <littlewangfei@gmail.com>

--- src/com/feihome/support/utils/CommonUtils.java
+++ src/com/feihome/support/utils/CommonUtils.java
@@ -1,5 +1,17 @@
 package com.feihome.support.utils;
 
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class CommonUtils {
+    public static String generateUUID() {
+        return StringUtils.replace(UUID.randomUUID().toString(), "-", "");
+    }
+    
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+            System.out.println(CommonUtils.generateUUID());
+        }
+    }
 }
