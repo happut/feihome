1ecb44716c6e721b2a83c3c2ac97f352105a5387
wangfei <littlewangfei@gmail.com>
2016-09-01 16:31:40 +0800

    根据新样式进行修改
    Signed-off-by: wangfei <littlewangfei@gmail.com>

--- src/com/feihome/support/polymerization/service/impl/BjdxNewsPolymerization.java
+++ src/com/feihome/support/polymerization/service/impl/BjdxNewsPolymerization.java
@@ -26,7 +26,7 @@ public class BjdxNewsPolymerization implements IPolymerization {
     @Qualifier("polymerizationDao")
     private PolymerizationDao polymerizationDao;
 
-    private String baseUrl = "http://www.bjdx.gov.cn/zmhd/wsgssx/";
+    private String baseUrl = "http://www.bjdx.gov.cn/jrdx/dxxw/zwxx/";
 
     private String articleUrl = "http://www.bjdx.gov.cn";
 
@@ -38,23 +38,23 @@ public class BjdxNewsPolymerization implements IPolymerization {
     public List<TPolymerizationElement> getPagableTPolymerizationElements(
             String baseUrl, int page) {
         List<TPolymerizationElement> dataList = new ArrayList<TPolymerizationElement>();
-        boolean flag = false;
+        List<String> orginids = new ArrayList<String>();
         try {
             String spiderUrl = "";
             // 抽取当前页
             if (page == 0) {
                 spiderUrl = baseUrl;
             } else {
-                spiderUrl = baseUrl + "index" + page + ".htm";
+                spiderUrl = baseUrl + "index" + page + ".html";
             }
             Document doc = Jsoup.connect(spiderUrl).get();
-            Elements select = doc.select(".zfwj_ul li");
+            Elements select = doc.select(".article_list li");
             for (Element e : select) {
                 TPolymerizationElement element = new TPolymerizationElement();
 
-                String url = e.child(0).attr("href");
-                String date = e.child(1).text().replace("[", "")
+                String date = e.child(0).text().replace("[", "")
                         .replace("]", "");
+                String url = e.child(1).attr("href");
 
                 try {
                     element.setDate(DateUtils.parseDate(date,
@@ -63,7 +63,7 @@ public class BjdxNewsPolymerization implements IPolymerization {
                     element.setDate(null);
                 }
 
-                element.setTitle(e.child(0).attr("title"));
+                element.setTitle(e.child(1).attr("title"));
                 element.setType(1);
                 if (!url.startsWith("http://")) {
                     element.setUrl(articleUrl + "/" + url);
@@ -73,13 +73,16 @@ public class BjdxNewsPolymerization implements IPolymerization {
                     element.setUrl(url);
                     element.setOrginId("" + page + e.elementSiblingIndex());
                 }
+                orginids.add(element.getOrginId());
                 dataList.add(element);
             }
 
             // 如果未查找完毕 继续查找下一页
-            if (!flag) {
-                Elements changepage = doc.select(".changepage li a");
-                Element a = changepage.last();
+            if (polymerizationDao
+                    .getPolymerizationElementsCountByTypeAndOrginIds(1,
+                        orginids.toArray(new String[] {})) == 0) {
+                Elements changepage = doc.select(".pagination a");
+                Element a = changepage.get(changepage.size() - 2);
                 String nextUrl = a.attr("href");
                 if (page == NumberUtils.toInt(nextUrl.substring(
                     nextUrl.lastIndexOf("index") + "index".length(),
